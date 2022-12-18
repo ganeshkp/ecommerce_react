a184c7c5ff8dd5c029454271de8a000ed17662f5
ganeshkp <ganeshkumar.patil@gmail.com>
2022-12-18 18:54:16 +0000

    Backend user authentication modified

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from .models import Product
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -75,21 +73,3 @@ def getUsers(request):
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    # product=None
-
-    # for i in products:
-    #     if i['_id'] == pk:
-    #         product = i
-    #         break
-
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-    return Response(serializer.data)
